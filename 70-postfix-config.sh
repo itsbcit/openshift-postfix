a6fb5d4d2b3798647427da5a67a5196f139dd744
Jesse Weisner <jesse_weisner@bcit.ca>
2019-04-24 15:28:26 -0700

    Basic postfix container

---
+++ 70-postfix-config.sh
@@ -0,0 +1,28 @@
+if [ "$EUID" != "0" ];then
+    echo 'This container must run as root :(' >&2
+    exit 255
+fi
+
+[ -f ${CONFIGDIR}/aliases ] || cp /etc/aliases ${CONFIGDIR}/aliases
+[ -f ${CONFIGDIR}/main.cf ] || tar zxf /postfix-config.tar.gz -C ${CONFIGDIR}
+
+if [ ! -d ${MAILSPOOLDIR} ];then
+    mkdir -p ${MAILSPOOLDIR}
+    chown postfix:postfix ${MAILSPOOLDIR}
+    chmod 700 ${MAILSPOOLDIR}
+fi
+
+if [ ! -d ${QUEUEDIR} ];then
+    mkdir -p ${QUEUEDIR}
+    chown postfix:postfix ${QUEUEDIR}
+    chmod 700 ${QUEUEDIR}
+    tar zxf /postfix-spool.tar.gz -C ${QUEUEDIR}
+fi
+
+if [ ! -d ${DATADIR} ];then
+    mkdir -p ${DATADIR}
+    chown postfix:postfix ${DATADIR}
+    chmod 700 ${DATADIR}
+fi
+
+[ -f /etc/postfix/aliases ] && /usr/bin/newaliases
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # openshift-postfix
 Postfix container for OpenShift
+
+This container must be run with ANYUID privileges.
