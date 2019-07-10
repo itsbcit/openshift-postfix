85d81ee596b572cbe382da4341d95f14003735e2
Juraj Ontkanin <Juraj_Ontkanin@bcit.ca>
2019-07-10 16:44:13 -0700

    ignore binary files when dockerizing

--- 70-postfix-config.sh
+++ 70-postfix-config.sh
@@ -31,7 +31,7 @@ if [ -f "$CONFIGDIR"/.DOCKERIZE.env ]; then
     echo "loading: ${CONFIGDIR}/.DOCKERIZE.env"
     . "$CONFIGDIR"/.DOCKERIZE.env
 fi
-for config_file in $( find /etc/postfix -type f -not -path '*/\.git/*' ); do 
+for config_file in $( find /etc/postfix -type f -not -path '*/\.git/*' -exec grep -Iq . {} \; -print ); do 
     echo "dockerizing: $config_file"
     dockerize -template "$config_file":"$config_file"
 done
